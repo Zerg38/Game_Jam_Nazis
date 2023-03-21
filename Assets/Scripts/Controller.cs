7e1728bcfe1d2124a623e259e7661692f6f9eba7
VoIdLaX27 <alex.taravilla@enti.cat>
2023-03-21 17:24:58 +0100

    salto

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -12,9 +12,6 @@ public class Controller : MonoBehaviour
     public GameObject groundRayObject;
     public LayerMask groundMask;
     public LayerMask wallMask;
-    public Vector3 Roffset;
-    public Vector3 Loffset;
-
 
     // Start is called before the first frame update
     void Start()
@@ -33,19 +30,10 @@ public class Controller : MonoBehaviour
         grounded = false;
         for (int i = 0; i < points.Count; i++)
         {
-            Debug.DrawRay(groundRayObject.transform.position + points[i], -transform.up, Color.red);
+            Debug.DrawRay(groundRayObject.transform.position + points[i], -transform.up ,Color.red);
             RaycastHit2D hitGround = Physics2D.Raycast(groundRayObject.transform.position + points[i], -transform.up, 1.1f, groundMask);
             grounded |= hitGround.collider != null;
 
         }
-
-
-        Debug.DrawRay(groundRayObject.transform.position + Roffset, -transform.right, Color.magenta);
-        RaycastHit2D hitWall = Physics2D.Raycast(groundRayObject.transform.position + Roffset, -transform.right, 1f, wallMask);
-
-        Debug.DrawRay(groundRayObject.transform.position + Loffset, transform.right, Color.magenta);
-        RaycastHit2D hitWall_2 = Physics2D.Raycast(groundRayObject.transform.position + Loffset, transform.right, 1f, wallMask);
-
-        walled = hitWall.collider != null || hitWall_2.collider != null;
     }
 }
