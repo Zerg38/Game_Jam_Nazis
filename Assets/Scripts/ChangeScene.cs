e63e8744c7744e1c255e9eac9072790a6e22afbd
PC-imatge-2022-2023 <licenses@enti.cat>
2023-03-22 10:43:33 +0100

    palette clouds

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void Game(int sceneID)
     {
-        
+        SceneManager.LoadScene(sceneID);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Quit()
     {
-        
+        Application.Quit();
     }
+
+
 }
